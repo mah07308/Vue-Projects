fc3a124cc67fec4b06a2bc3ac7ab2dc8c4f055f9
Mohammed Ahmad Hussain <mah07308@gmail.com>
2021-02-21 14:55:37 +0530

    Added todo course successfully

---
+++ vue_crash_todolist/src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <todo-item
+        v-bind:todo="todo"
+        v-on:del-todo="$emit('del-todo', todo.id)"
+      ></todo-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  name: 'Todos',
+  components: { TodoItem },
+  props: ['todos'],
+};
+</script>
+
+<style scoped></style>
